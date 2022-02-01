d6aa4032b8aa4bc4d616571510073ee85c60c1cc
alladmitrievna87 <98809957+alladmitrievna87@users.noreply.github.com>
2022-02-01 11:55:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("           Lab work №1");
+            Console.WriteLine("");
+            Console.WriteLine("  Console appendix structure in С#");
+            Console.WriteLine("");
+            Console.WriteLine("      Pinchuk Alla Dmytrivna");
+            Console.WriteLine("       KI-241, 172 specialty");
+            Console.WriteLine("");
+            Console.WriteLine("            Variant 1");
+            Console.WriteLine("");
+            Console.WriteLine("Task 2");
+            Console.WriteLine("");
+            double x = 0.31; // first value
+            double y = -0.0049;
+            double A = Math.Sqrt(Math.Log10(4 / 3 + x)) - Math.Pow(Math.E, - Math.Sin(1.3 * x - 0.7));// math expression
+            Console.WriteLine($" x = {x}, y = {y}, A = {A}");// output result
+            double x1 = 2.5;// second value
+            double A1 = Math.Sqrt(Math.Log10(4 / 3 + x1)) - Math.Pow(Math.E, - Math.Sin(1.3 * x1 - 0.7)); //math expression
+            Console.WriteLine($" x1 = {x1}, y = {y}, A1 = {A1}"); //output result
+                                
+        }
+    }
+}
